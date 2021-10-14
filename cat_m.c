759172d86eab6f48b3bf4e280c37883c9a8c2f67
zooneon <snsxhdz@naver.com>
2021-10-14 21:11:20 +0900

    cat

---
+++ cat_m.c
@@ -0,0 +1,20 @@
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(int argc, char **argv) {
+        char buf[512];
+        int fd, n;
+
+        fd = open(argv[1], O_RDONLY);
+        if(fd == -1) {
+                return;
+        }
+
+        while((n=read(fd, buf, 512)) != 0) {
+                write(1, buf, n);
+        }
+
+        return 0;
+}
