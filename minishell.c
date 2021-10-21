9fc42d3b3ea81bcdc41da0e77a0dd9a1c46a2dcc
Junwon Kwon <snsxhdz@naver.com>
2021-10-21 21:39:10 +0900

    minishell.c

--- minishell.c
+++ minishell.c
@@ -12,37 +12,37 @@ int main(void){
 		pid_t pid;
 
         while (1){
-			getcwd(name, 99);
-			printf("%s> ", name);
+		getcwd(name, 99);
+		printf("%s> ", name);
 
         	gets(in);
-			if (in[0]=='\0')
-				continue;
+		if (in[0]=='\0')
+			continue;
 
-			i=0;
+		i=0;
         	res[i]=strtok(in, " ");
-			while (res[i]){
-				res[++i]=strtok(NULL, " ");
-			}
+		while (res[i]){
+			res[++i]=strtok(NULL, " ");
+		}
 
         	if (strcmp(res[0], "exit")==0){
         		exit(0);
-			}
-			else if (strcmp(res[0], "cd_m")==0){
-				chdir(res[1]);
-			}
-			else {
-				switch(pid=fork()) {
-					case -1:
-						perror("fork failed");
-						break;
-					case 0:
-						execvp(res[0], res);
-					default:
-						wait(&status);
-				}
+		}
+		else if (strcmp(res[0], "cd_m")==0){
+			chdir(res[1]);
+		}
+		else {
+			switch(pid=fork()) {
+				case -1:
+					perror("fork failed");
+					break;
+				case 0:
+					execvp(res[0], res);
+				default:
+					wait(&status);
 			}
 		}
+	}
 
-		return 0;
+	return 0;
 }
